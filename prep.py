a0e1221b15987840a6f998347fd3e144ba4a2c8d
Masha Medvedeva <vera.vtraktor@gmail.com>
2017-01-15 16:01:16 +0100

    clean documented preprocessing file

--- prep.py
+++ prep.py
@@ -1,69 +1,43 @@
-from nltk import bigrams, trigrams, ngrams
 import re, os, random
 import glob
-
-
-#def extract_labels(lang, lang_words):
-#
-#	labels = []
-#	for num in range(len(lang_words)):
-#		labels.append(lang)
-#
-#	return labels
-
+#pseudo-random, keep the number constant to be able to compare results
+random.seed(132)
 
 def extract_words_and_labels(mode):
 	all_words = []
 	all_labels = []
 	all_lines = []
 	for file_name in glob.glob('*' + mode + '.txt'):
-		#lang = os.path.basename(file_name)[:-4]
-		#print('Processing', lang)
 		lang_words = []
 		labels = []
-		#lang_char = []
 		f = open(file_name, 'r')
 		print(file_name)
 		for line in f:
 			line = re.sub('\n', '', line)
 			line = re.sub('\r\n', '', line)
-			#line = re.sub('	', ' ', line)
-			line = line.split('	')
+			line = line.split('	') #split by the tab between the line and the language
+			#the line without the language
 			lang_words.extend(line[:-1])
-			#lang_char.extend([i for i in line])
-
-		#i2 = []
-		#lang_char_4 = list(ngrams(lang_char,3))
-		#for i in lang_char_4:
-		#	i = list(i)
-		#	i = ''.join(i)
-		#	i2.append(i)
-#
-		#lang_char = list(ngrams(i2,5))
-
+			## split the lines by space, strip of everythin, lower, bla bla and reassemble again?
+			#language
 			lang = line[-1]
-			#print(lang)
 			
-
 			for num in range(len(line)-1):
 				labels.append(lang)
 		all_words.extend(lang_words)
-
 		all_labels.extend(labels)
-		#print(all_labels)
-	print("Available:", len(all_words))
+
 	assert(len(all_labels)==len(all_words))
 
 
 	for n in range(len(all_labels)):
 		line = (all_words[n], all_labels[n])
 		all_lines.append(line)
+	#shuffle the lines for training, otherwise it learns 'bs' only first and then 'hr' only	
 	random.shuffle(all_lines)
-	##list(all_lines)
 	all_words = []
 	all_labels = []
 	for i in all_lines:
-		#print (i[0], i[1])
 		all_words.append(i[0])
 		all_labels.append(i[1])
 
@@ -81,13 +55,9 @@ def extract_words_and_labels(mode):
 			group_labels.append('group5')
 		if l == 'es-ES' or l == 'es-AR' or l == 'es-PE':
 			group_labels.append('group6')
-		#else:
-		#	print('ERROR:', l)
-	print('EXAMPLE:', group_labels[0])
 
-
-	return all_words[:5000], group_labels[:5000], all_labels[:5000]
-
-#def extract_words_and_labels(mode):
-#	return extract_words(mode)
-
+	#Xtrain, Ytrain, Ztrain
+	#sentence, group, language
+	return all_words, group_labels, all_labels
+	#return all_words[:5000], group_labels[:5000], all_labels[:5000]
+	#try out on smaller dataset, 5000 is the length of test data
